6da7ac66c4d7e56529359ddb38fa103b46c7795e
Nishan Niraula <nishan@amity.co>
2025-03-13 23:11:22 +0700

    Release 7.0.0 bare

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "AmitySDK",
-            targets: ["AmitySDK", "Realm", "RealmSwift"]
+            targets: ["AmitySDK"]
         ),
     ],
     targets: [
@@ -16,16 +16,6 @@ let package = Package(
             name: "AmitySDK",
             url: "https://sdk.amity.co/sdk-release/ios-frameworks/7.0.0/AmitySDK.xcframework.zip",
             checksum: "d120f90a414998bba4b26d50e77f90e7cb44acb4177fc1c5b32617e4d0d7f42e"
-        ),
-        .binaryTarget(
-            name: "Realm",
-            url: "https://sdk.amity.co/sdk-release/ios-frameworks/7.0.0/Realm.xcframework.zip",
-            checksum: "45f7f7fcdf5aa381c45c2c22ed289b683db1cb39470113abdf18f4cdbac9bff8"
-        ),
-        .binaryTarget(
-            name: "RealmSwift",
-            url: "https://sdk.amity.co/sdk-release/ios-frameworks/7.0.0/RealmSwift.xcframework.zip",
-            checksum: "a0cc63112276e93afb52fee3ef053e98da9e051ef5cde742dc8d24806658049d"
-        ),
+        )
     ]
 )
